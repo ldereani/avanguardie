fdaf7b98783350c0b0d0affd16982ef349ebefdc
ldereani <56175546+ldereani@users.noreply.github.com>
2022-08-11 19:33:45 +0200

    Aggiunta iniziale
    Aggiunta iniziale

---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Avanguardie
+ */
+$theme_locations = get_nav_menu_locations();
+?>
+<!doctype html>
+<html lang="it">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
